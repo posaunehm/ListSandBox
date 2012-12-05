6cfc75ef14195b35bc34ee358a5de9683380b8ab
posaunehm <posaunehm@gmail.com>
2012-12-05 12:29:30 +0900

    Image+テキストの形式に変更した。

--- ListboxSandboxApp/ViewModels/MainWindowViewModel.cs
+++ ListboxSandboxApp/ViewModels/MainWindowViewModel.cs
@@ -54,19 +54,26 @@ namespace ListboxSandboxApp.ViewModels
                 new ObservableCollection<ListBoxItemSampleViewModel>
                     {
                         new ListBoxItemSampleViewModel(
-                            Properties.Resources.Win8.GetHbitmap().ToBitmapSource()),
+                            Properties.Resources.Win8.GetHbitmap().ToBitmapSource())
+                            {Description = "Windows 8"},
                         new ListBoxItemSampleViewModel(
-                            Properties.Resources.Win7.GetHbitmap().ToBitmapSource()),
+                            Properties.Resources.Win7.GetHbitmap().ToBitmapSource())
+                            {Description = "Windows 7"},
                         new ListBoxItemSampleViewModel(
-                            Properties.Resources.WinVista.GetHbitmap().ToBitmapSource()),
+                            Properties.Resources.WinVista.GetHbitmap().ToBitmapSource())
+                            {Description = "Windows Vista"},
                         new ListBoxItemSampleViewModel(
-                            Properties.Resources.WinXP.GetHbitmap().ToBitmapSource()),
+                            Properties.Resources.WinXP.GetHbitmap().ToBitmapSource())
+                            {Description = "Windows XP"},
                             new ListBoxItemSampleViewModel(
-                                Properties.Resources.Win98.GetHbitmap().ToBitmapSource()),
+                                Properties.Resources.Win98.GetHbitmap().ToBitmapSource())
+                            {Description = "Windows 98"},
                         new ListBoxItemSampleViewModel(
-                            Properties.Resources.Win95.GetHbitmap().ToBitmapSource()),
+                            Properties.Resources.Win95.GetHbitmap().ToBitmapSource())
+                            {Description = "Windows 95"},
                         new ListBoxItemSampleViewModel(
-                            Properties.Resources.WinNT.GetHbitmap().ToBitmapSource()),
+                            Properties.Resources.WinNT.GetHbitmap().ToBitmapSource())
+                            {Description = "Windows NT"},
                     };
         }
 
